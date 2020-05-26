245f868d54b1dcc6e2b082e3155ca02a098263f3
Raju Dawadi <rajudawadinp@gmail.com>
2020-05-26 22:35:42 +0545

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,7 +21,6 @@ sonar-scanner \
 	-Dsonar.projectKey=${PWD##*/} \
 	-Dsonar.projectBaseDir=${INPUT_PROJECTBASEDIR} \
 	-Dsonar.login=${INPUT_LOGIN} \
-	-Dsonar.password=${INPUT_PASSWORD} \
 	-Dsonar.sources=. \
 	-Dsonar.sourceEncoding=UTF-8 \
 	${SONAR_PASSWORD}
