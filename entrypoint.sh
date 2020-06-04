87ad748743980fb56c33d1b84c38d4d4bbf69405
Raju Dawadi <rajudawadinp@gmail.com>
2020-06-04 23:16:42 +0545

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -22,7 +22,7 @@ sonar-scanner \
 	-Dsonar.projectBaseDir=${INPUT_PROJECTBASEDIR} \
 	-Dsonar.login=${INPUT_LOGIN} \
 	-Dsonar.sources=. \
-	-Dsonar.sourceEncoding=UTF-8
+	-Dsonar.sourceEncoding=UTF-8 | echo ::set-output name=status::ERROR
 apk add jq
 cat report-task.txt
 SONAR_RESULT="report-task.txt"
